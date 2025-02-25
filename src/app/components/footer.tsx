df43c8ced260e9027c66fc6dec4c2dfb49e1ddf1
Joaquin Terrado Mora <joacoterrado@gmail.com>
2025-02-25 17:42:18 +0100

    Update footer links to point to specific social media profiles and add target attributes for improved accessibility

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,14 +6,14 @@ export const Footer = async () => {
         <footer className="bg-white py-6">
             <div className="container mx-auto px-4 text-center">
                 <div className="flex justify-center space-x-6 mb-4">
-                    <Link href="https://www.instagram.com" aria-label="Instagram" className="text-gray-900 hover:text-gray-400 transition-colors">
+                    <Link href="https://www.instagram.com/pixelar.es/" aria-label="Instagram" target= "_blank" rel="noopener noreferrer" className="text-gray-900 hover:text-gray-400 transition-colors">
                         <FaInstagram size={24} />
                     </Link>
-                    <Link href="https://www.tiktok.com" aria-label="TikTok" className="text-gray-900 hover:text-gray-400 transition-colors">
+                    <Link href="https://www.tiktok.com/@pixelar.es?_t=ZM-8uDOej0PEgx&_r=1" aria-label="TikTok" target= "_blank" rel="noopener noreferrer" className="text-gray-900 hover:text-gray-400 transition-colors">
                         <FaTiktok size={22} />
                     </Link>
                 </div>
-                <p className="text-gray-400">Powered by <Link href="https://pixelar.es" className="text-gray-900 hover:text-gray-400 transition-colors">Pixelar</Link></p>
+                <p className="text-gray-400">Powered by <Link href="https://pixelar.es" target= "_blank" rel="noopener noreferrer" className="text-gray-900 hover:text-gray-400 transition-colors">Pixelar</Link></p>
             </div>
         </footer>
     )
